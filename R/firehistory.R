09354c31f55b06bcd4be4661261d4e4a0d97ce11
Brewster Malevich <sbmalev@gmail.com>
2015-06-07 14:14:56 -0700

    Initial package commit.

--- R/firehistory.R
+++ R/firehistory.R
@@ -1,21 +1,3 @@
-#   Copyright 2011-2014 S. Brewster Malevich <malevich@email.arizona.edu>
-#
-#   This is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>
-
-require("ggplot2")
-require("reshape2")
-
 fhx <- function(year,  series, type, metalist=list()){
   # Constructor for S3 fhx class.
   if (!is.numeric(year)) stop("year must be numeric")

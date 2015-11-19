71da72a1924c91a69170c5ab27b464eabee570b2
Brewster Malevich <sbmalev@gmail.com>
2015-11-19 14:21:57 -0700

    Removed more non-ascii characters.

--- R/data.R
+++ R/data.R
@@ -18,7 +18,7 @@
 
 #' Peggy Mesa fire-history data
 #'
-#' An fhx object with fire-history data from Peggy Mesa. See dataset `pgm_meta` for metadata. Data from Guiterman, Christopher H., Ellis Q. Margolis, and Thomas W. Swetnam. 2015. "Dendroecological Methods For Reconstructing High-Severity Fire In Pine-Oak Forests." Tree-Ring Research 71 (2): 67–77. doi:10.3959/1536-1098-71.2.67.
+#' An fhx object with fire-history data from Peggy Mesa. See dataset `pgm_meta` for metadata. Data from Guiterman, Christopher H., Ellis Q. Margolis, and Thomas W. Swetnam. 2015. "Dendroecological Methods For Reconstructing High-Severity Fire In Pine-Oak Forests." Tree-Ring Research 71 (2): 67-77. doi:10.3959/1536-1098-71.2.67.
 
 #'
 #' @format An fhx object with 37 series from 1555 to 2013 CE. Each series is a string with 5 characters.
@@ -26,7 +26,7 @@
 
 #' Metadata for the Peggy Mesa fire-history dataset
 #'
-#' A dataset with species and location information for the Peggy Mesa fire-history dataset (`pgm`). Data from Guiterman, Christopher H., Ellis Q. Margolis, and Thomas W. Swetnam. 2015. "Dendroecological Methods For Reconstructing High-Severity Fire In Pine-Oak Forests." Tree-Ring Research 71 (2): 67–77. doi:10.3959/1536-1098-71.2.67.
+#' A dataset with species and location information for the Peggy Mesa fire-history dataset (`pgm`). Data from Guiterman, Christopher H., Ellis Q. Margolis, and Thomas W. Swetnam. 2015. "Dendroecological Methods For Reconstructing High-Severity Fire In Pine-Oak Forests." Tree-Ring Research 71 (2): 67-77. doi:10.3959/1536-1098-71.2.67.
 #'
 #' @format A data frame with 45 rows and 5 variables:
 #' \itemize{
